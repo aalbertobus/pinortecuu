033bd94bfe50f1c8a6be57b8ce72d0f97586d681
Magio <aalbertobus@gmail.com>
2013-08-27 15:42:35 -0600

    webpage

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+
+$(function(){
+	$("#container").fadeIn(1000);
+	$("footer").slideDown(800);
+	$("#contact_container").slideDown(1000);
+	$(".info_content").slideDown(1000);
+	$("#twhash_container").slideUp(function(){
+		$("#twhash_container").fadeIn(2500);
+	});
+	$(".sponsor_img").fadeIn(3000);
+});	
